f96741124f2016c6b6a2c452eb40c52421069124
Papa-Johnson <jumbeekari@gmail.com>
2024-12-15 22:54:15 +0200

    15 Dec update

--- Quiz Page/quizscript.js
+++ Quiz Page/quizscript.js
@@ -5,6 +5,9 @@ const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
 
 
 startBtn.onclick = () => {
@@ -27,6 +30,34 @@ continueBtn.onclick = () => {
     questionCounter(1);
     headerScore();
 }
+
+tryAgainBtn.onclick = () => {
+    quizBox.classList.add('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+
+    headerScore();
+}
+goHomeBtn.onclick = () => {
+    quizSection.classList.remove('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+
+    headerScore();
+}
+
 let questionCount = 0;
 let questionNumb = 1;
 let userScore = 0;
@@ -44,7 +75,7 @@ nextBtn.onclick = () => {
         nextBtn.classList.remove('active');
     }
     else{
-        console.log('Questions completed');
+        showResultsBox();
     }
 }
 
@@ -63,14 +94,14 @@ function showQuestions(index) {
     optionList.innerHTML = optionFlag;
 
     const option = document.querySelectorAll('.option');
-    const options = document.querySelectorAll('.option'); // Select all option elements
+    const options = document.querySelectorAll('.option'); 
 
     for (let i = 0; i < options.length; i++) {
     options[i].setAttribute('onclick', 'optionSelected(this)');
     }
 }
 function optionSelected(answer) {
-    let userAnswer = answer.textContent; // Get the text content of the selected answer
+    let userAnswer = answer.textContent;
     let correctAnswer = questions[questionCount].answer;
     let allOptions = optionList.children.length;
 
@@ -104,3 +135,28 @@ function headerScore() {
     const headerScoreText = document.querySelector('.header-score');
     headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
 }
+function showResultsBox() {
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active'); 
+  
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent =`Your score is ${userScore}  out of ${questions.length}`;
+  
+    const circularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+  
+    let progressStartValue = -1; 
+    let progressEndValue = Math.round((userScore / questions.length) * 100); 
+    let speed = 20;
+  
+    let progress = setInterval(() => {
+      progressStartValue++; 
+
+      progressValue.textContent = `${progressStartValue}%`;
+      circularProgress.style.background = `conic-gradient(#002ec4 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+  
+      if (progressStartValue === progressEndValue) {
+        clearInterval(progress); 
+      }
+    }, speed);
+  }
